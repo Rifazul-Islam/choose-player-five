a0dd6d4ed9d959c56f423e66127b114b02783767
Rifazul <rifazul50@gmail.com>
2022-08-22 11:47:23 +0600

    add oder list item

---
+++ js/child.js
@@ -0,0 +1,56 @@
+
+
+
+document.getElementById('btn-item1').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+    const li = document.createElement('li');
+    li.innerText = 'Linoel messi';
+    listContainer.appendChild(li);
+})
+
+
+
+document.getElementById('btn-item2').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+    const li2 = document.createElement('li');
+    li2.innerText = 'Mo Salah';
+    listContainer.appendChild(li2);
+})
+
+
+document.getElementById('btn-item3').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+    const li3 = document.createElement('li');
+    li3.innerText = 'Ronaldo dos';
+    listContainer.appendChild(li3);
+})
+
+
+document.getElementById('btn-item4').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+    const li4 = document.createElement('li');
+    li4.innerText = 'Mesut Ozil';
+    listContainer.appendChild(li4);
+})
+
+
+document.getElementById('btn-item5').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+    const li5 = document.createElement('li');
+    li5.innerText = 'Kylian mbappé';
+    listContainer.appendChild(li5);
+})
+
+
+document.getElementById('btn-item6').addEventListener('click', function () {
+
+    const listContainer = document.getElementById('list-container');
+    const li6 = document.createElement('li');
+    li6.innerText = 'Adama Traore';
+    listContainer.appendChild(li6);
+})
